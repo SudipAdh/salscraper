babcca317438ff2ba72d355e46014a63370be4cd
Daradji Salah Eddine <saledddar@gmail.com>
2018-07-15 08:54:01 +0100

    Edit: setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup, find_packages
 import sys, os
 
-README = open('README.rst').read()
+README = open('README.md').read()
 
 setup(name='piescraper',
-    version='0.1',
-    description='',
+    version='0.0.1',
+    description='A scarping tool',
     long_description=README,
     long_description_content_type='text/markdown',
-    classifiers=[],
-    keywords='',
-    author='',
-    author_email='',
-    url='E:\CPs\Projects\version-control\Ongoing',
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: Microsoft :: Windows',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+    keywords='scrape crawl',
+    author='saledddar',
+    author_email='saledddar@gmail.com',
+    url='https://github.com/Saledddar/piescraper',
     license='MIT',
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    install_requires=[]
+    packages = ['piescraper'],
+    install_requires=['saltools','pyunet']
 )
