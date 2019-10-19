871e6afb24029190a52344dd2d5b8de09e156d3e
Daradji Salah Eddine <saledddar@gmail.com>
2019-10-19 17:07:10 +0100

    RLS: 0.1.3

--- setup.py
+++ setup.py
@@ -1,28 +1,31 @@
-from setuptools import setup, find_packages
-import sys, os
+from    setuptools  import  setup   , find_packages
+from    os.path     import  join    , dirname         , abspath
+import  sys
 
-README = open('README.md').read()
+BASE_DIR    = abspath(dirname(__file__))
+README      = open(join(BASE_DIR, 'README.rst')).read()
+REQS        = [line.strip() for line in open(join(BASE_DIR, 'requirements.txt')).readlines() if line.strip()]
 
 setup(
-    name                            = 'salscraper'                              ,
-    version                         = '0.0.2'                                   ,
-    description                     = 'A scarping tool'                         ,
-    long_description                = README                                    ,
-    long_description_content_type   = 'text/markdown'                           ,
+    name                            = 'salscraper'                                  ,
+    version                         = '0.0.3'                                       ,
+    description                     = 'A scarping tool'                             ,
+    long_description                = README                                        ,
+    long_description_content_type   = 'text/markdown'                               ,
     classifiers                     = [
-          'Development Status :: 3 - Alpha'                                     ,
-          'Intended Audience :: Developers'                                     ,
-          'License :: OSI Approved :: MIT License'                              ,
-          'Operating System :: Microsoft :: Windows'                            ,
-          'Programming Language :: Python'                                      ,
-          'Topic :: Software Development :: Libraries :: Python Modules'        ],
-    keywords                        = 'scrape crawl'                            ,
-    author                          = 'saledddar'                               ,
-    author_email                    = 'saledddar@gmail.com'                     ,
-    url                             = 'https://github.com/Saledddar/salscraper' ,
-    license                         = 'MIT'                                     ,
-    include_package_data            = True                                      ,
-    zip_safe                        = False                                     ,
-    packages                        = ['salscraper']                            ,
-    package_dir                     = {'': 'src'}                               ,
-    install_requires                = ['saltools']                              )
+          'Development Status :: 3 - Alpha'                                         ,
+          'Intended Audience :: Developers'                                         ,
+          'License :: OSI Approved :: MIT License'                                  ,
+          'Operating System :: Microsoft :: Windows'                                ,
+          'Programming Language :: Python'                                          ,
+          'Topic :: Software Development :: Libraries :: Python Modules'            ],
+    keywords                        = 'scrape crawl'                                ,
+    author                          = 'saledddar'                                   ,
+    author_email                    = 'saledddar@gmail.com'                         ,
+    url                             = 'https://github.com/Saledddar/salscraper'     ,
+    license                         = 'MIT'                                         ,
+    include_package_data            = True                                          ,
+    zip_safe                        = False                                         ,
+    packages                        = ['salscraper']                                ,
+    package_dir                     = {'': 'src'}                                   ,
+    install_requires                = [REQS]                                        )
